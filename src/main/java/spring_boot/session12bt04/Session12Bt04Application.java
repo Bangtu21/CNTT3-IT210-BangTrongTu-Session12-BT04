6175dde9481c5569086e261c3f23966f3d84a311
bangtrongtu <bangtu2005.com@gmail.com>
2026-04-21 22:29:17 +0700

    br04

---
+++ src/main/java/spring_boot/session12bt04/Session12Bt04Application.java
@@ -0,0 +1,13 @@
+package spring_boot.session12bt04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Session12Bt04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Session12Bt04Application.class, args);
+    }
+
+}
---
+++ src/test/java/spring_boot/session12bt04/Session12Bt04ApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_boot.session12bt04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Session12Bt04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
